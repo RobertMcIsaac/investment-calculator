441c29df0954db0b0de2ad119133d1bd6c4d353d
Robert McIsaac <robmcisaac@hotmail.co.uk>
2025-12-02 15:13:41 +0000

    feat: add header

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule],
+})
+export class AppModule {}
+
+// header component
+
+// user-input component
+
+// investment-results component
